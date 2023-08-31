61129f0e329664524fcd25d8f9e791cc5c35bcca
DreamSky1996 <webhipe@gmail.com>
2023-08-31 09:42:07 +0900

    fix some errors

--- api/index.py
+++ api/index.py
@@ -1,46 +1,64 @@
-from flask import Flask
-from binance import Client
-from telegram.ext import Updater, CallbackContext, CommandHandler, MessageHandler, Filters
-from telegram import Update
-from dotenv import load_dotenv
 import re
-import os
+from flask import Flask, request
+import telegram
+from telebot.credentials import bot_token, bot_user_name,URL
+
+global bot
+global TOKEN
+TOKEN = bot_token
+bot = telegram.Bot(token=TOKEN)
 
 app = Flask(__name__)
-load_dotenv()
+
 @app.route('/')
 def home():
     return 'Hello, World!'
 
-TELEGRAM_BOT_TOKEN = GCP_PROJECT_ID = os.getenv('TELEGRAM_BOT_TOKEN')
-updater = Updater(TELEGRAM_BOT_TOKEN, use_context=True)
+@app.route('/{}'.format(TOKEN), methods=['POST'])
+def respond():
+   # retrieve the message in JSON and then transform it to Telegram object
+   update = telegram.Update.de_json(request.get_json(force=True), bot)
 
-def get_price(_symbol):
-	price = '';
-	try:
-		client = Client()
-		client.API_URL = 'https://api.binance.com/api'
-		price += f'{_symbol}: $' + str(float(client.get_symbol_ticker(symbol=f'{_symbol}USDT')['price']))
-	except Exception as e:
-		price += 'Not found this coin'
-		# print(f'Error: {e}')
+   chat_id = update.message.chat.id
+   msg_id = update.message.message_id
 
-	return price
+   # Telegram understands UTF-8, so encode text for unicode compatibility
+   text = update.message.text.encode('utf-8').decode()
+   # for debugging purposes only
+   print("got text message :", text)
+   # the first time you chat with the bot AKA the welcoming message
+   if text == "/start":
+       # print the welcoming message
+       bot_welcome = """
+       Welcome to coolAvatar bot, the bot is using the service from http://avatars.adorable.io/ to generate cool looking avatars based on the name you enter so please enter a name and the bot will reply with an avatar for your name.
+       """
+       # send the welcoming message
+       bot.sendMessage(chat_id=chat_id, text=bot_welcome, reply_to_message_id=msg_id)
 
-def start(update: Update, context: CallbackContext):
-	update.message.reply_text(
-        "Hello sir, Welcome to the Bot.Please write\
-        coin Symbol to see Price.")
 
-def coin_price(update: Update, context: CallbackContext):
-	symbol = re.sub(r"[^a-zA-Z0-9]","",update.message.text).upper()
-	price = get_price(symbol)
-	update.message.reply_text(price)
+   else:
+       try:
+           # clear the message we got from any non alphabets
+           text = re.sub(r"\W", "_", text)
+           # create the api link for the avatar based on http://avatars.adorable.io/
+           url = "https://api.adorable.io/avatars/285/{}.png".format(text.strip())
+           # reply with a photo to the name the user sent,
+           # note that you can send photos by url and telegram will fetch it for you
+           bot.sendPhoto(chat_id=chat_id, photo=url, reply_to_message_id=msg_id)
+       except Exception:
+           # if things went wrong
+           bot.sendMessage(chat_id=chat_id, text="There was a problem in the name you used, please enter different name", reply_to_message_id=msg_id)
 
-updater.dispatcher.add_handler(CommandHandler('start', start))
-updater.dispatcher.add_handler(MessageHandler(Filters.text, coin_price))
-updater.start_polling()
-# updater.idle()
+   return 'ok'
+
+@app.route('/set_webhook', methods=['GET', 'POST'])
+def set_webhook():
+   s = bot.setWebhook('{URL}{HOOK}'.format(URL=URL, HOOK=TOKEN))
+   if s:
+       return "webhook setup ok"
+   else:
+       return "webhook setup failed"
+
 
 if __name__ == '__main__':
-    app.run(threaded=True)
+   app.run(threaded=True)
