36ac2c178531b89a81ace539933826e5395ec2ad
DreamSky1996 <webhipe@gmail.com>
2023-09-01 15:05:53 +0900

    add callback query

--- api/index.py
+++ api/index.py
@@ -49,36 +49,48 @@ def home():
 def respond():
    # retrieve the message in JSON and then transform it to Telegram object
    update = telegram.Update.de_json(request.get_json(force=True), bot)
+   if update.message != None:
+        chat_id = update.message.chat.id
+        # msg_id = update.message.message_id
 
-   chat_id = update.message.chat.id
-   # msg_id = update.message.message_id
-
-   # Telegram understands UTF-8, so encode text for unicode compatibility
-   text = update.message.text.encode('utf-8').decode()
-   # for debugging purposes only
-   print("got text message :", text)
-   # the first time you chat with the bot AKA the welcoming message
-   if text == "/start":
-       # print the welcoming message
-       bot_welcome = """
-       Welcome to Coin Price Bot, the bot is using the service from Binance.com to get Coin price
-       """
-       # send the welcoming message
-       # bot.sendMessage(chat_id=chat_id, text=bot_welcome, reply_to_message_id=msg_id)
-       bot.sendMessage(chat_id=chat_id, text=bot_welcome)
-   elif text == "/coins":
-        # token_list = get_token_list()
-        keyboard = [[telegram.InlineKeyboardButton("ETH", callback_data='ETH'), telegram.InlineKeyboardButton("BNB", callback_data='BNB'),telegram.InlineKeyboardButton("BTC", callback_data='BTC')]]
-        reply_markup = telegram.InlineKeyboardMarkup(keyboard)
-        bot.sendMessage(chat_id=chat_id, text="Coins", reply_markup=reply_markup)
-   else:
+        # Telegram understands UTF-8, so encode text for unicode compatibility
+        text = update.message.text.encode('utf-8').decode()
+        # for debugging purposes only
+        print("got text message :", text)
+        # the first time you chat with the bot AKA the welcoming message
+        if text == "/start":
+            # print the welcoming message
+            bot_welcome = """
+            Welcome to Coin Price Bot, the bot is using the service from Binance.com to get Coin price
+            """
+            # send the welcoming message
+            # bot.sendMessage(chat_id=chat_id, text=bot_welcome, reply_to_message_id=msg_id)
+            bot.sendMessage(chat_id=chat_id, text=bot_welcome)
+        elif text == "/coins":
+                # token_list = get_token_list()
+                keyboard = [[telegram.InlineKeyboardButton("ETH", callback_data='ETH'), telegram.InlineKeyboardButton("BNB", callback_data='BNB'),telegram.InlineKeyboardButton("BTC", callback_data='BTC')]]
+                reply_markup = telegram.InlineKeyboardMarkup(keyboard)
+                bot.sendMessage(chat_id=chat_id, text="Coins", reply_markup=reply_markup)
+        else:
+            try:
+                symbol = re.sub(r"[^a-zA-Z0-9]","",text).upper()
+                price = get_price(symbol)
+                bot.sendMessage(chat_id=chat_id, text=price)
+            except Exception:
+                # if things went wrong
+                bot.sendMessage(chat_id=chat_id,text="Please type corret coin symbol")
+        return 'ok'
+   if update.callback_query != None:
+       callback_query_id = update.callback_query.id
+       data = update.callback_query.data.encode('utf-8').decode()
+       print("data", data)
        try:
-           symbol = re.sub(r"[^a-zA-Z0-9]","",text).upper()
+           symbol = re.sub(r"[^a-zA-Z0-9]","",data).upper()
            price = get_price(symbol)
-           bot.sendMessage(chat_id=chat_id, text=price)
+           bot.answerCallbackQuery(callback_query_id=callback_query_id, text=price)
        except Exception:
            # if things went wrong
-           bot.sendMessage(chat_id=chat_id,text="Please type corret coin symbal")
+           bot.answerCallbackQuery(callback_query_id=callback_query_id, text="Please type corret coin symbal")
 
    return 'ok'
 
